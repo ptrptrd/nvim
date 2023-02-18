4ae0a6a8d819ec3b8e44f4f3bfd22ab78c728638
pp7rd <pptrd@protonmail.com>
2023-02-18 17:27:24 +0000

    basic configuration for nvim

---
+++ lua/set.lua
@@ -0,0 +1,14 @@
+vim.opt.guicursor		= ""
+vim.opt.hlsearch		= false
+vim.opt.incsearch		= true
+vim.opt.nu				= true
+vim.opt.tabstop			= 4
+vim.opt.termguicolors	= true
+vim.opt.relativenumber	= true
+vim.opt.scrolloff		= 10
+vim.opt.shiftwidth		= 4
+vim.opt.smartindent		= true
+vim.opt.softtabstop		= 4
+vim.opt.wildmode		= "full"
+vim.opt.wildignorecase  = true
+vim.opt.wrap			= false
---
+++ plugins/plugins.lua
@@ -0,0 +1,71 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  
+  -- Color Theme
+  use({
+    'navarasu/onedark.nvim',
+    as = 'onedark',
+    config = function()
+      require('onedark').setup{
+        style = 'warm'
+      }
+      vim.cmd('colorscheme onedark')
+   end
+  })
+
+  -- LSP Configuration & Plugins
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {'williamboman/mason.nvim'},           -- Optional
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},         -- Required
+		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
+		  {'hrsh7th/cmp-buffer'},       -- Optional
+		  {'hrsh7th/cmp-path'},         -- Optional
+		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
+		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},             -- Required
+		  {'rafamadriz/friendly-snippets'}, -- Optional
+	  }
+  }
+
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Treesitter
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+
+  -- Which Key
+  use {
+   "folke/which-key.nvim",
+    config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+    end
+  }
+
+end)
